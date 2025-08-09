299c27bc4afea43bbfbf366da8a2e44b2c92f4dd
Himanshu Bharambe <himanshubharambe04@gmail.com>
2025-08-09 12:50:59 +0530

    Lower and Upper Bound

---
+++ Binary Search/Lower_&_Upper_Bound.py
@@ -0,0 +1,45 @@
+# Lower and Upper Bound
+
+# Lower Bound [smallest index such that nums[i]>= target]
+
+nums = [1,1,2,3,4,5,6,6,7,8,9]
+target = 2
+
+def lowerbound(nums,target):
+    n = len(nums)
+    low = 0
+    high = n-1
+    lb = n
+    while low <= high:
+        mid = (low+high) // 2
+        if nums[mid] >= target:
+            lb = mid
+            high = mid - 1
+        
+        else:
+            low = mid + 1
+    return lb
+
+print(lowerbound(nums,target))
+
+
+# Upper Bound
+nums = [1,1,2,3,4,5,6,6,7,8,9]
+target = 2
+
+def lowerbound(nums,target):
+    n = len(nums)
+    low = 0
+    high = n-1
+    ub = n
+    while low <= high:
+        mid = (low+high) // 2
+        if nums[mid] > target:
+            ub = mid
+            high = mid - 1
+        
+        else:
+            low = mid + 1
+    return ub
+
+print(lowerbound(nums,target))
