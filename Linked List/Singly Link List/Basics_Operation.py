fffee7e39a2b049b6c39734f404d5b4ea0052cb4
Himanshu Bharambe <himanshubharambe04@gmail.com>
2025-08-13 09:47:22 +0530

    Basic Operations - Insert, Delete, Append, Traverse

--- Linked List/Singly Link List/Basics_Operation.py
+++ Linked List/Singly Link List/Basics_Operation.py
@@ -17,6 +17,9 @@ class SinglyLinkList:
             while curr.next is not None:
                 curr = curr.next
             curr.next = new_node
+
+
+
 # Traversal Operation  TC -> o(N)     SC -> o(1)
     def traverse(self):
         if self.head is None:
@@ -27,9 +30,58 @@ class SinglyLinkList:
                 print(curr.val, end= " ")
                 curr = curr.next
                 print()
+
+
+# Insert Operation   
+    def insert(self,val,position):
+        new_node = Node(val)
+        # If We want to insert at head node
+        if position == 0:
+            new_node.next = self.head
+            self.head = new_node
+        # If We want to insert at specific position
+        else:
+            current = self.head
+            prev_node = None
+            count = 0
+            while current is not None and count < position:
+                prev_node = current
+                current = current.next
+                count += 1
+            prev_node.next = new_node
+            new_node.next = current
+
+
+# Delete Operation
+    def delete(self,val):
+        # If we want to delete at head node
+        temp = self.head       # Assign Temp variable to head
+        if temp.next is not None:
+            if temp.val == val:
+                self.head = temp.next
+                return
+            else:
+                found = False   # If value doesnt exits in list
+                prev = None
+                while temp is not None:
+                    if temp.val == val:
+                        found = True
+                        break
+                    prev = temp
+                    temp = temp.next
+                if found:
+                    prev.next = temp.next
+                    return
+                else:
+                    print("Node not found")
+
+
 sll = SinglyLinkList()
 sll.append(10)
 sll.append(20)
 sll.append(30)
 sll.append(40)
+sll.insert(100,2)
+sll.delete(20)
 sll.traverse()
+
