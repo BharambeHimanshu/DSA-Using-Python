5d8a2eb56a09cec945093677541e4fb2dba17aa5
Himanshu Bharambe <himanshubharambe04@gmail.com>
2025-09-22 09:11:46 +0530

    Binary Tree Representation

--- Binary Trees/Learn Traversal/Binary Tree Representation.py
+++ Binary Trees/Learn Traversal/Binary Tree Representation.py
@@ -1,1 +1,32 @@
 # Binary Tree Representation
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+r"""
+       Drinks
+        /   \
+      Hot   Cold
+     / \    /  \
+   Coffee Tea Cola Fanta
+"""
+
+drinks = Node("Drinks")
+Hot = Node("Hot")
+Cold = Node("Cold")
+Coffee = Node("Coffee")
+Tea = Node("Tea")
+Cola = Node("Cola")
+Fanta = Node("Fanta")
+
+drinks.left = Hot
+drinks.right = Cold
+Hot.left = Coffee
+Hot.right = Tea
+Cold.left = Cola
+Cold.right = Fanta
+
+print(drinks.left.right.val)  # Tea
